3dc5435884b7ec729d397b0df831d674e21fdadb
basokant <basokanthan@gmail.com>
2022-10-31 16:35:56 -0400

    finish group 4 instruction function headers

--- src/Processor.h
+++ src/Processor.h
@@ -101,6 +101,91 @@ private:
      */
     void ret();
 
+    /**
+     * @brief Stores the address in register i.
+     * 
+     * @param i number of the register (0x-0xf for v0-vf)
+     * @param addr A 16-bit address in memory
+     */
+    void load_addr(uint8_t i, uint16_t addr);
+
+
+    /**
+     * @brief The program jumps by setting the program counter to an address (addr plus the value of v0)
+     * 
+     * @param v0 number of the register (0x0-0xf for v0-vf)
+     * @param addr A 16-bit address in memory
+     */
+    void jump(uint8_t v0, uint16_t addr);
+
+    
+    /**
+     * @brief Generate a random number from 0 to 255, which is then ANDed with a value byte, and stored in a register vx.
+     * 
+     * @param vx number of the register (0x0-0xf for v0-vf)
+     * @param byte 8-bit value (0x00-0xff)
+     */
+    void random(uint8_t vx, uint8_t byte);
+
+
+    /**
+     * @brief Display n-byte sprite starting at memory location I at (vx, vy), set VF = collision.
+     * 
+     * @param vx number of the register (0x0-0xf for v0-vf)
+     * @param vy number of the register (0x0-0xf for v0-vf)
+     * @param nibble 4-bit value (0x0-0xf)
+     */
+    void display(uint8_t vx, uint8_t vy, uint8_t nibble);
+
+    
+    /**
+     * @brief Skip next instruction if key with the value of vx is pressed.
+     * 
+     * @param vx number of the register (0x0-0xf for v0-vf)
+     */
+    void skip_pressed(uint8_t vx);
+
+
+    /**
+     * @brief Skip next instruction if key with teh value of vx is NOT pressed.
+     * 
+     * @param vx number of the register (0x0-0xf for v0-vf)
+     */
+    void skip_not_pressed(uint8_t vx);
+
+
+    /**
+     * @brief Set register vx to the value of the delay timer.
+     * 
+     * @param vx number of the register (0x0-0xf for v0-vf)
+     */
+    void load_register_from_dt(uint8_t vx);
+
+
+    /**
+     * @brief Wait for a key press, store the value of the key in register vx.
+     * 
+     * @param vx number of the register (0x0-0xf for v0-vf)
+     * @param key value of the key (0x0-0xf)
+     */
+    void load_key(uint8_t vx, uint16_t key);
+
+
+    /**
+     * @brief Set delay timer to the value of register vx
+     * 
+     * @param vx number of the register (0x0-0xf for v0-vf)
+     */
+    void load_dt_from_register(uint8_t vx);
+
+
+    /**
+     * @brief Set the sound timer to the value of register vx
+     * 
+     * @param vx number of the register (0x0-0xf for v0-vf)
+     */
+    void load_st_from_register(uint8_t vx);
+
 private:
     /*
     v0-vF: 16 8-bit registers
