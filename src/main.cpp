060d7ba3444a8c6ab3b852b90ceb279a40d19e9f
David Tran <davidtranhq@gmail.com>
2022-11-08 22:20:26 -0500

    Add test assembly program

--- src/main.cpp
+++ src/main.cpp
@@ -6,11 +6,11 @@
 
 int main() {
     Processor processor;
-    // sequence of instructions to run
-    // TODO: write in assembly
+    // sequence of test instructions to run
     std::array<uint8_t, 0x10000> instructions = {
-        0x12, // LOAD 5 into A
-        0x23, // LOAD 56
+        0x6011, // ld v0, 0x11
+        0x6122, // ld v1, 0x22
+        0x8014 // add v0, v1
     };
 
     // CPU dump BEFORE running the instructions
