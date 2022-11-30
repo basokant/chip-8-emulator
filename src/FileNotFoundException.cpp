04beb98d3e2750795ea11f0ac531b0c1dd8eaa31
davidtranhq <davidtranhq@gmail.com>
2022-11-29 19:53:06 -0500

    Add comment headers

--- src/FileNotFoundException.cpp
+++ src/FileNotFoundException.cpp
@@ -1,7 +1,23 @@
+/**
+ * @file FileNotFoundException.cpp
+ * @author David Tran, Ben Asokanthan, Dhruv Panicker, Davis Mtui, Sean Hou
+ * @brief Implementation for the FileNotFoundException
+ * @version 0.1
+ * @date 2022-11-08
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
 #include <stdexcept>
 #include <string>
 
 #include "FileNotFoundException.h"
 
-FileNotFoundException::FileNotFoundException():runtime_error("File not found") {}
-FileNotFoundException::FileNotFoundException(std::string msg):runtime_error(msg.c_str()) {}
+FileNotFoundException::FileNotFoundException()
+    : runtime_error("File not found")
+{}
+
+FileNotFoundException::FileNotFoundException(std::string msg)
+    : runtime_error(msg.c_str())
+{}
--- src/FileNotFoundException.h
+++ src/FileNotFoundException.h
@@ -1,3 +1,14 @@
+/**
+ * @file FileNotFoundException.h
+ * @author David Tran, Ben Asokanthan, Dhruv Panicker, Davis Mtui, Sean Hou
+ * @brief Interface for a custom exception for missing files
+ * @version 0.1
+ * @date 2022-11-08
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
 #ifndef FILE_NOT_FOUND_EXCEPTION_H_INCLUDED
 #define FILE_NOT_FOUND_EXCEPTION_H_INCLUDED
 
